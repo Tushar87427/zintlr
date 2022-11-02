67ab5201c4b25f33d6e303f7236c7992320c632b
Tusharsoni <tusharsoni0101@gmail.com>
2022-11-02 12:53:48 +0530

    data added to json file

--- getlatka/spiders/datascraping.py
+++ getlatka/spiders/datascraping.py
@@ -1,43 +1,27 @@
-
-
 import scrapy
-
-
-
 class DatascrapingSpider(scrapy.Spider):
     name = 'datascraping'
     allowed_domains = ['www.getlatka.com']
-    start_urls = ['https://getlatka.com/saas-companies?page=1']
+    start_urls = ['https://getlatka.com/saas-companies?page=%d' % j for j in range(1,1310)]
 
 
     def parse(self, response):
         for i in response.xpath('//*[@class="data-table_table__SwBLY"]//tbody/tr'):
-            # temp  =response.xpath('//*[@class="data-table_table__SwBLY"]//tbody/tr')
 
 
 
-        # name = response.xpath("(//tr/td[2]/div/section/a)/text()").get()
             yield{
-                'company_name ':  i.xpath("(td[2]/div/section/a)/text()").extract_first(),
-                'revenue_name': i.xpath("(td[3]/p[1]/text())").extract()[1].strip()
+                'company_name ':  i.xpath("(td[2]/div/section/a)/text()").extract_first() if  i.xpath("(td[2]/div/section/a)/text()").extract_first() else  " ",
+                'revenue': i.xpath("(td[3]/p[1]/text())").extract()[1].strip() if i.xpath("(td[3]/p[1]/text())").extract()[1].strip() else " ",
+                'funding': i.xpath("(td[4]/p[1]/text())").extract()[1].strip() if i.xpath("(td[4]/p[1]/text())").extract()  else "" ,
+                'Valuation': i.xpath("(td[5]/p[1]/text())").extract()[1].strip() if i.xpath("(td[5]/p[1]/text())").extract() else " ",
+                'funder': i.xpath("(td[7]/div/a/text())").extract_first() if i.xpath("(td[7]/div/a/text())").extract_first() else " ",
+                "Team Size" : i.xpath("(td[8]/p[1]/text())").extract()[1].strip() if i.xpath("(td[8]/p[1]/text())").extract()[1].strip() else " "
             }
 
 
 
-        # for company in response.xpath("//tr/td/section/a "):
-            # name = response.xpath(".//text()").get()
-            # response.css(".table.table-striped > tr:nth-child(1) > td:nth-child(2)//div[@class='cells_cell-1__9cP8o']/ul").get()
-
-            # table = response.selector('//*[@class="data-table_table__SwBLY"]//tbody')
 
 
 
 
-
-
-            # yield{
-            #     "Company " :table
-            #     # "Revenue" :Revenue ,
-            #     # "Funding" :Funding ,
-            #     # "Valuation":Valuation
-            # }
